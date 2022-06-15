23d92704848423c355bc67b71ec63c00a63716a0
Adrian Lugo <misteralugo@gmail.com>
2022-06-15 13:52:31 -0500

    testing moved tag to _document

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="L8vVyvBwYScCo7cR-ZNfeiOCeek097r6yyvWBp5pRb0" />
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home({ data }) {
         <title>Interactive Comments Section</title>
         <meta name="description" content="Create, Read, Update, and Delete Comments" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="google-site-verification" content="L8vVyvBwYScCo7cR-ZNfeiOCeek097r6yyvWBp5pRb0" />
       </Head>
 
       <App data={myData} setData={setMyData} />
